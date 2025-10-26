66e0bb0e20a1ac8a5f31673a8017232def60b40a
Lathesparan Ramachandran <lathesparan@corzent.com>
2025-10-26 14:15:45 +0530

    Fix port configurations for all services - use correct ports (3002, 3003, 3005)

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'http://18.141.4.247', // Frontend URL
   services: {
-    userService: 'http://13.214.136.130:3000/api/users',
-    bookingService: 'http://13.214.201.141:3000/api/bookings', 
-    codeReviewService: 'http://3.1.51.192:3000/api/reviews'
+    userService: 'http://13.214.136.130:3005/api/users',
+    bookingService: 'http://13.214.201.141:3002/api/bookings', 
+    codeReviewService: 'http://3.1.51.192:3003/api/reviews'
   }
 };
